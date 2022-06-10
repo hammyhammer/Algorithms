815b32ca3e435a7a42ad0320f05338c68bb2605e
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-10 17:43:19 -0400

    did palindrome, comments, ad compexity

--- JavaScript_Algos/palindrome.js
+++ JavaScript_Algos/palindrome.js
@@ -8,12 +8,20 @@ const isPalindrome = (string1, string2) => {
     return false
   }
 
-
+  // Create two variables of our input strings as reversed versions.
   let reverse1 = string1.split('').reverse().join('');
   let reverse2 = string2.split('').reverse().join('');
-  console.log(reverse1.length)
-  console.log(reverse2)
+
+  // If string1 is equal to the reverse version of string2. Likewise
+  // Vice versa, return true. Else we will return false. 
+  if (string1 === reverse2 || string2 === reverse1) {
+    return true
+  }
 
 }
 
-console.log(isPalindrome("hello", "hallo"))
+console.log(isPalindrome("kayak", "kayak"))
+
+// The complexity would be 0(n) because of the linear relationship
+// of how long the strings would be. The reverse method has a 
+// linear complexity associated with it. 
