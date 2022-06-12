112ce11ecf9aa1a722319ce35e78b06b18b62ef2
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-12 11:37:03 -0400

    solving new approach to twosum

--- JavaScript_Algos/twosum.js
+++ JavaScript_Algos/twosum.js
@@ -2,6 +2,23 @@
 // array of numbers contains two elements that can add up to the 
 // target sum. 
 
+const sumTwo = (nums, target) => {
+  let complementary = new Map();
+
+  for (let i = 0; i < nums.target; i++) {
+    if (complementary[nums[i]] >= 0) {
+      return [complementary[nums[i]], i]
+    }
+    else {
+      complementary[target - nums[i]] = i
+    }
+  }
+}
+
+console.log(sumTwo([1, 5, 9, 10], 14))
+
+/////////////////////////////////////////////////
+
 const twoSum = (nums, target) => {
   // We can iterate over the array. We can compare different numbers
   // to one another to see if they add up to the target number
