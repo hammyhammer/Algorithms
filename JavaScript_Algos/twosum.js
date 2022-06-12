f29c5082dd8bb76cbda6a2794c79cd3de018aa58
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-11 21:46:22 -0400

    added complexity and explaination

--- JavaScript_Algos/twosum.js
+++ JavaScript_Algos/twosum.js
@@ -3,6 +3,8 @@
 // target sum. 
 
 const twoSum = (nums, target) => {
+  // We can iterate over the array. We can compare different numbers
+  // to one another to see if they add up to the target number
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -14,3 +16,6 @@ const twoSum = (nums, target) => {
 }
 
 console.log(twoSum([1, 5, 9, 10], 19))
+
+// The complexity for this solution is O(n^2) because of the nested
+// for loop.
