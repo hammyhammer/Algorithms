952dd03a97fece2c677fdd7f17dd9c14f7f85caa
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-11 21:25:27 -0400

    created a twosum file

---
+++ JavaScript_Algos/twosum.js
@@ -0,0 +1,7 @@
+// Given an array of numbers, and a target value. Determine if the
+// array of numbers contains two elements that can add up to the 
+// target sum. 
+
+const twoSum = (nums, target) => {
+
+}
