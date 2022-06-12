d58615da7141063cf0256fd539512a604899415c
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-11 21:41:53 -0400

    find a solution for twosum

--- JavaScript_Algos/twosum.js
+++ JavaScript_Algos/twosum.js
@@ -3,8 +3,14 @@
 // target sum. 
 
 const twoSum = (nums, target) => {
-
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
 
 }
 
-twoSum([1, 5, 10], 15)
+console.log(twoSum([1, 5, 9, 10], 19))
