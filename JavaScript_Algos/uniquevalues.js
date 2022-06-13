77dc5451a39ac1ae25c5fbebc1e6a05880aad6d5
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-13 12:31:58 -0400

    found a solution

--- JavaScript_Algos/uniquevalues.js
+++ JavaScript_Algos/uniquevalues.js
@@ -1,5 +1,19 @@
 // Given a sorted array, count the unqiue values in the array.
 
 const countUniqueValues = (array) => {
+  let count = {};
 
+  for (let element of array) {
+    if (count[element]) {
+      count[element]++
+    }
+    else {
+      count[element] = 1
+    }
+  }
+  // console.log(count)
+  // console.log(Object.keys(count).length)
+  return Object.keys(count).length
 }
+
+console.log(countUniqueValues([1, 1, 2, 3, 4, 5, 6, 6]))
