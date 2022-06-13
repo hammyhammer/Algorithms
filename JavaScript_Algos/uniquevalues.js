059863c29b9dcb0fbba761095d5429661f4f6373
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-13 12:43:41 -0400

    provided explanation and complexity

--- JavaScript_Algos/uniquevalues.js
+++ JavaScript_Algos/uniquevalues.js
@@ -1,19 +1,30 @@
 // Given a sorted array, count the unqiue values in the array.
 
 const countUniqueValues = (array) => {
+  // We create a empty object.
   let count = {};
 
+  // With a for of loop, we record how many times a value appears
+  // and store it as a key value pair within our count object. 
+
   for (let element of array) {
-    if (count[element]) {
+    // If a value already exists in our object, we increment it by
+    // one. 
+    if (count[element] !== undefined) {
       count[element]++
     }
+    // Else we create a new key and give it a value of one. 
     else {
       count[element] = 1
     }
   }
-  // console.log(count)
-  // console.log(Object.keys(count).length)
+
+  // We return the length of the keys within count to determine how
+  // many unique values exist. 
   return Object.keys(count).length
 }
 
+// The compexity for this solution would be O(n), because we still
+// have to go through th entire array to ensure we get all the
+// values. 
 console.log(countUniqueValues([1, 1, 2, 3, 4, 5, 6, 6]))
