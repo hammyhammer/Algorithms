89d4e60333932b0e2b0bf9d8a2bf050adc61fb0b
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-15 22:23:36 -0400

    completed binary search

--- JavaScript_Algos/searchingalgos.js
+++ JavaScript_Algos/searchingalgos.js
@@ -31,5 +31,19 @@ const binarySearch = (array, value) => {
   // (In the event that the array has an even number of elements)
   let middle = Math.floor((start + end) / 2);
 
-
+  while (array[middle] !== value && start <= end) {
+    if (value < array[middle]) {
+      end = middle - 1;
+    }
+    else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (array[middle] === value) {
+    return middle
+  }
+  return -1
 }
+
+console.log(binarySearch([1, 2, 4, 6, 8, 10, 14, 18, 21], 7))
