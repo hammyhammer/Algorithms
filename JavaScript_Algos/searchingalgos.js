7d4a8548c74d0ffe451b98ca628259a747fde74c
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-15 22:11:55 -0400

    added binary search function and added extra details to linear search

--- JavaScript_Algos/searchingalgos.js
+++ JavaScript_Algos/searchingalgos.js
@@ -1,7 +1,7 @@
 // This file contains different methods for searching through arrays.
 
 // LINEAR SEARCH
-// Finding a value within an array
+// Finding a value within an array be tranversing the entire array
 const linearSearch = (array, value) => {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === value) {
@@ -14,3 +14,12 @@ const linearSearch = (array, value) => {
 // This is an example of a linear complexity. We are going through
 // the entire array to see if we can find a match for the value. 
 console.log(linearSearch([1, 2, 3, 4, 5, 6], 7))
+
+
+// BINARY SEARCH
+// The idea we want to do with a binary search is to "cut out" 
+// parts of the array that we do not care about. 
+// **This way only works for sorted arrays** 
+const binarySearch = (array, value) => {
+
+}
