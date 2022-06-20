e8334dd8feacf20c0693a59a9ebb9d7f55bce8c9
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-19 22:07:08 -0400

    added explanation and complexity

--- JavaScript_Algos/lastword.js
+++ JavaScript_Algos/lastword.js
@@ -1,11 +1,16 @@
 // Given a string. Determine the number of letters in the last word.
 
 const lastWord = (string) => {
+  // Create  a variable to remove the white space within the string
   let sentence = string.trim()
+
+  // Create an array version of the string. ['Hello', 'there', "Anakin"]
   let array = sentence.split(' ')
+
+  // Remove the last word in the string and intitaze it as a variable. 
   let final = array.pop()
   final.split('')
   return final.length
 }
-
+// This solution has a complex of O(1) Because we are accessing the last word in the array.
 console.log(lastWord('Hello there, Anakin'))
