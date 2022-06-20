f6dee7faa8d6e51faa3a7d254a96454071e2f36c
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-19 22:01:46 -0400

    found solution for last word

--- JavaScript_Algos/lastword.js
+++ JavaScript_Algos/lastword.js
@@ -1,1 +1,11 @@
 // Given a string. Determine the number of letters in the last word.
+
+const lastWord = (string) => {
+  let sentence = string.trim()
+  let array = sentence.split(' ')
+  let final = array.pop()
+  final.split('')
+  return final.length
+}
+
+console.log(lastWord('Hello there, Anakin'))
