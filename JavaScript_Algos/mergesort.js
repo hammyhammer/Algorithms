72cb94c46519f0f79c3ea075590d5c66cd23468b
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-21 15:33:56 -0400

    complexity and explanation

--- JavaScript_Algos/mergesort.js
+++ JavaScript_Algos/mergesort.js
@@ -2,10 +2,13 @@
 // or are empty
 // Once we have them, we merge those arrays with other sorted arrays
 function merge(array1, array2) {
+
+  // We create an empty array to push our values in order into.
   let results = [];
   let i = 0;
   let j = 0;
 
+  // Determining what values will be pushed into the results array
   while (i < array1.length && j < array2.length) {
     if (array2[j] > array1[i]) {
       results.push(array1[i]);
@@ -16,6 +19,8 @@ function merge(array1, array2) {
       j++;
     }
   }
+
+  // Logic for if one array runs out of elements but not the other, just to push them into results
   while (i < array1.length) {
     results.push(array1[i]);
     i++;
@@ -27,4 +32,19 @@ function merge(array1, array2) {
   return results
 }
 
+function mergeSort(array) {
+  // Cutting the array until it is one or less
+  if (array.length <= 1) return array
+  // Cutting it in the middle of the array
+  let middle = Math.floor(array.length / 2)
+  let left = mergeSort(array.slice(0, middle))
+  let right = mergeSort(array.slice(middle))
+  // Sorts and then merge the arrays 
+  return merge(left, right)
+}
+console.log(mergeSort([10, 24, 76, 73, 72, 1, 9]))
 
+// The complexity for merge sort is O(n log n). Whenever we split
+// Depending on how many elements are in an array, we are asking the 
+// exponent value to reach the total length. O(log n). But we are still
+// going through all the elements to compare so O(n)
