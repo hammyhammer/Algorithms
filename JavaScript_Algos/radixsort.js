87e013f55ec597a50a5955b84080da10c8aa0577
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-29 14:42:06 -0400

    added getdigit function

--- JavaScript_Algos/radixsort.js
+++ JavaScript_Algos/radixsort.js
@@ -1,1 +1,10 @@
 // Unique because it does not make comparsions between elements
+// Special way to sort numbers by looking at how many digits a number
+// has.
+
+// HELPER FUNCTION
+function getDigit(number, i) {
+  return Math.floor(Math.abs(number) / Math.pow(10, i)) % 10;
+}
+
+console.log(getDigit(7323, 2))
