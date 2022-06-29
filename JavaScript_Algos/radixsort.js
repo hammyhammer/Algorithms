cfaf6a6388d40f57a735623dce9e07c992abb4fa
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-29 14:45:50 -0400

    added digitcount function

--- JavaScript_Algos/radixsort.js
+++ JavaScript_Algos/radixsort.js
@@ -7,4 +7,9 @@ function getDigit(number, i) {
   return Math.floor(Math.abs(number) / Math.pow(10, i)) % 10;
 }
 
+function digitCount(number) {
+  if (number === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(number))) + 1;
+}
+
 console.log(getDigit(7323, 2))
