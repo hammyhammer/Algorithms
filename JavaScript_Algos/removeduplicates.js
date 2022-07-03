1596aa42d1831bcc301a2c124f50a971a35d9a5d
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-03 10:09:44 -0400

    added solution to removeDuplicates

--- JavaScript_Algos/removeduplicates.js
+++ JavaScript_Algos/removeduplicates.js
@@ -1,3 +1,14 @@
 // Given an integer array that is sorted in an increasing order,
 // remove duplicates in-place so that we are left with unique values. 
 // Do not make another array for the solution
+
+function removeDuplicates(nums) {
+  let counter = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[counter] = nums[i]
+      counter++
+    }
+  }
+  return counter
+}
