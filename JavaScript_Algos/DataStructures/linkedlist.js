afdc5ac8018df36fad60c2058c58a8ef78bda81d
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-04 08:51:23 -0400

    added some code and a skeleton to push linkedlists

--- JavaScript_Algos/DataStructures/linkedlist.js
+++ JavaScript_Algos/DataStructures/linkedlist.js
@@ -2,3 +2,15 @@
 // Nodes pointing to another node. Think of it like a train.
 // Unlike arrays, they do not have indexes. Random acess is not allowed
 // No indexes means insertion and deletion is cheap (do not have to reindex everything).
+
+
+class Node {
+  constructor(val) {
+    this.value = value;
+    this.next = null
+  }
+}
+
+let first = new Node('Hello');
+first.next = new Node('there')
+first.next.next = new Node('Anakin')
