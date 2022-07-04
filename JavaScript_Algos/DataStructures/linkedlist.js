2fe78048ea6403959ce681cd4d0261a249f7d8e1
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-04 08:44:25 -0400

    added linkedlist file and some info

---
+++ JavaScript_Algos/DataStructures/linkedlist.js
@@ -0,0 +1,4 @@
+// Contains nodes. Contains head, tail, and length property.
+// Nodes pointing to another node. Think of it like a train.
+// Unlike arrays, they do not have indexes. Random acess is not allowed
+// No indexes means insertion and deletion is cheap (do not have to reindex everything).
