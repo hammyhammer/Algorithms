24c3e11455f9fe5e76ecc39b0891abae8f49a6b5
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-04 09:25:08 -0400

    added pop

--- JavaScript_Algos/DataStructures/linkedlist.js
+++ JavaScript_Algos/DataStructures/linkedlist.js
@@ -11,6 +11,59 @@ class Node {
   }
 }
 
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = head;
+    this.tail = tail;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++
+    return this
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--
+    return current;
+
+  }
+
+
+
+  // tranverse() {
+  //   let current = this.head;
+  //   while (current) {
+  //     console.log(current.val)
+  //     current = current.next
+  //   }
+  // }
+}
+
+//This method below is not optimal because of all the .next.next
 let first = new Node('Hello');
 first.next = new Node('there')
 first.next.next = new Node('Anakin')
+first.next.next.next = new Node('Skywalker')
+
+//
+let list = new SinglyLinkedList()
+list.push('Hello')
+list.push('THere')
