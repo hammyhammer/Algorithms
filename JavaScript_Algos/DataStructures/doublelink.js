ec5cd3e5fb3fde518894f1ed7d2169efa537ac68
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-05 11:44:23 -0400

    added skeleton to DLL

--- JavaScript_Algos/DataStructures/doublelink.js
+++ JavaScript_Algos/DataStructures/doublelink.js
@@ -1,2 +1,18 @@
 // Though it is more flexible than Singly Linked Lists, Doubly Linked Lists take more memory.
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
