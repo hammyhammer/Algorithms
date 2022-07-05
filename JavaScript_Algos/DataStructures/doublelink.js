0435bdb69b6b22e46a8480b7897f8ba4e706efce
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-05 12:04:39 -0400

    added push and pop

--- JavaScript_Algos/DataStructures/doublelink.js
+++ JavaScript_Algos/DataStructures/doublelink.js
@@ -14,5 +14,41 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode;
+    }
+    this.length++
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--
+    return poppedNode
+  }
 }
 
+let list = new DoublyLinkedList()
+list.push(1)
+list.push(2)
+list.push(3)
+list.pop()
+console.log(list)
