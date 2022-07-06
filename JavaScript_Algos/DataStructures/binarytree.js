5ade2201b3bf9efdb309c615814d30c8890902d4
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-06 17:32:26 -0400

    separated traversal code

--- JavaScript_Algos/DataStructures/binarytree.js
+++ JavaScript_Algos/DataStructures/binarytree.js
@@ -73,6 +73,55 @@ class BinarySearchTree {
     return current;
   }
 
+
+  ///////////////TRANSVERSAL///////////////////
+
+  BFS() {
+    let node = this.root
+    let data = []
+    let queue = []
+    queue.push(this.root)
+    while (queue.length) {
+      node = queue.shift()
+      data.push(node.value)
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+    return data
+  }
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left)
+      if (node.right) traverse(node.right)
+    }
+    traverse(this.root)
+    return data
+  }
+
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      if (node.right) traverse(node.right)
+      data.push(node.value);
+    }
+    traverse(this.root)
+    return data
+  }
+
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      data.push(node.value);
+      if (node.right) traverse(node.right)
+    }
+    traverse(this.root)
+    return data
+  }
+
 }
 
 let tree = new BinarySearchTree();
@@ -92,7 +141,10 @@ tree.insert(11);
 tree.insert(2);
 tree.insert(16);
 tree.insert(7);
-console.log(tree.find(999))
+console.log(tree.BFS())
+console.log(tree.DFSPreOrder())
+console.log(tree.DFSPostOrder())
+
 // console.log(tree)
 
 // Both searching and insertion has a complexity of O(log n). Pretty solid. 
