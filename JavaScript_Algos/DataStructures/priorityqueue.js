595cc2e01bfb1aeab6e8ba44c502bcb9504a64d2
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-07 14:46:31 -0400

    added prioirty queue

--- JavaScript_Algos/DataStructures/priorityqueue.js
+++ JavaScript_Algos/DataStructures/priorityqueue.js
@@ -1,10 +1,12 @@
-class MaxBinaryHeap {
+class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
-  insert(element) {
-    this.values.push(element);
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority)
+
+    this.values.push(newNode);
     this.bubbleUp();
   }
   bubbleUp() {
@@ -13,21 +15,21 @@ class MaxBinaryHeap {
     while (index > 0) {
       let parentIndex = Math.floor((index - 1) / 2)
       let parent = this.values[parentIndex];
-      if (element <= parent) break
+      if (element.priority <= parent.priority) break
       this.values[parentIndex] = element;
       this.values[index] = parent
       index = parentIndex
     }
   }
 
-  extractMax() {
-    const max = this.values[0]
+  dequeue() {
+    const min = this.values[0]
     const end = this.values.pop()
     if (this.values.length > 0) {
       this.values[0] = end;
       this.sinkDown();
     }
-    return max
+    return min
   }
   sinkDown() {
     let index = 0;
@@ -41,15 +43,15 @@ class MaxBinaryHeap {
 
       if (leftChildIndex < length) {
         leftChild = this.values[leftChildIndex]
-        if (leftChild > element) {
+        if (leftChild.priority < element.priority) {
           swap = leftChildIndex;
         }
       }
       if (rightChildIndex < length) {
         rightChild = this.values[rightChildIndex]
         if (
-          (swap === null && rightChild > element) ||
-          (swap !== null && rightChild > leftChild)
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
         ) {
           swap = rightChildIndex
         }
@@ -64,14 +66,20 @@ class MaxBinaryHeap {
     }
   }
 
-
 }
 
-let heap = new MaxBinaryHeap()
-heap.insert(55)
-heap.insert(13)
-heap.insert(100)
-heap.insert(50)
-heap.extractMax()
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
 
-console.log(heap.values)
+let ER = new PriorityQueue()
+ER.enqueue('cold', 1)
+ER.enqueue('blunt injury', 5)
+ER.enqueue('concussion', 3)
+ER.dequeue()
+ER.dequeue()
+
+console.log(ER)
