121f31e44cbbc90d4ca90a7d091c41fe998b8591
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-11 10:22:00 -0400

    added complexity

--- JavaScript_Algos/groupanagrams.js
+++ JavaScript_Algos/groupanagrams.js
@@ -15,3 +15,7 @@ function groupAnagrams(strs) {
 }
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+// Time: O(n x n log n) because we are sorting the elements by ABC order first (n log n),
+// and also are going through the entire array (linear)
+// Space: O(n) because the amount of space to store the results is equal to how many are in the array.
