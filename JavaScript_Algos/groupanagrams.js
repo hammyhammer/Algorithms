3c372aade8276fe036d6c3bae9b21ca9628bbc34
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-11 10:36:17 -0400

    added explanation

--- JavaScript_Algos/groupanagrams.js
+++ JavaScript_Algos/groupanagrams.js
@@ -1,16 +1,24 @@
 function groupAnagrams(strs) {
+  // We will sort and form a hashtable to come to a solution.
+
+  // Below we create a new variable to make a new array with the elements sorted by alphabet.
   let sortedArray = strs.map(word => word.split('').sort().join())
 
   let map = {};
 
   for (let i = 0; i < strs.length; i++) {
+
+    // If the object does not have an occurance, create a new key with the sorted element, 
+    // and add the unsorted element as a value to it.
     if (!map[sortedArray[i]]) {
       map[sortedArray[i]] = [strs[i]]
+
+      // If it does exist, then add unsorted element to key as a value 
     } else {
       map[sortedArray[i]].push(strs[i])
     }
   }
-
+  // We just care about the values, so return the values
   return Object.values(map)
 }
 
