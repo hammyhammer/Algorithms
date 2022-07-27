10482d71b44b567cac55110f0e5b78377577a167
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-27 16:47:29 -0400

    add edge

--- JavaScript_Algos/DataStructures/graph.js
+++ JavaScript_Algos/DataStructures/graph.js
@@ -8,4 +8,11 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList(vertex1).push(vertex2);
+    this.adjacencyList(vertex2).push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2)
 }
