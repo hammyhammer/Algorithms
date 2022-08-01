82303572909b1a3363f232bc66c6cba1b9499136
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-01 12:10:33 -0400

    added recursive method in graph

--- JavaScript_Algos/DataStructures/graph.js
+++ JavaScript_Algos/DataStructures/graph.js
@@ -10,8 +10,8 @@ class Graph {
   }
 
   addEdge(vertex1, vertex2) {
-    this.adjacencyList(vertex1).push(vertex2);
-    this.adjacencyList(vertex2).push(vertex1);
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 
   removeEdge(vertex1, vertex2) {
@@ -32,4 +32,42 @@ class Graph {
     }
     delete this.adjacencyList[vertex]
   }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    let potato = this.adjacencyList
+
+      (function dfs(vertex) {
+        if (!vertex) return null
+        visited[vertex] = true
+        result.push(vertex)
+        potato[vertex].forEach(neighbor => {
+          if (!visited[neighbor]) {
+            return dfs(neighbor)
+          }
+        })
+      })(start)
+    return result
+
+  }
 }
+
+let graph = new Graph();
+
+graph.addVertex("A")
+graph.addVertex("B")
+graph.addVertex("C")
+graph.addVertex("D")
+graph.addVertex("E")
+graph.addVertex("F")
+
+graph.addEdge("A", "B")
+graph.addEdge("A", "C")
+graph.addEdge("B", "D")
+graph.addEdge("C", "E")
+graph.addEdge("D", "E")
+graph.addEdge("D", "F")
+graph.addEdge("E", "F")
+
+// console.log(graph)
+console.log(graph.depthFirstRecursive("A"))
