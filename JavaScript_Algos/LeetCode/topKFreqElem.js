90ddeb808fa3348606b88a85c52d8afae152eac9
hammyhammer <hammyjacobs@gmail.com>
2022-10-07 18:06:14 -0400

    added explanation code

--- JavaScript_Algos/LeetCode/topKFreqElem.js
+++ JavaScript_Algos/LeetCode/topKFreqElem.js
@@ -1,11 +1,15 @@
 // Given an array of integers and an integer K, return the K most frequent elements. 
 
 let topKFrequent = function (nums, k) {
+  // We create an empty object. We will go through the whole nums array and record the frequency 
+  // for each unique numbers. 
   let map = {};
+
   let results = []
-  if (nums.length === 0) {
-    return []
-  }
+  // Base condition, if nums is empty, return empty []
+  if (nums.length === 0) return results;
+
+  // How we add and record our unique numbers and their frequency. 
   for (let element of nums) {
     if (map[element]) {
       map[element] += 1
@@ -15,14 +19,15 @@ let topKFrequent = function (nums, k) {
     }
   }
 
+  // Sorting our object from most frequent to least. 
   let array = Object.entries(map)
   array.sort((a, b) => {
     return b[1] - a[1]
   })
 
+  // Slicing the array to whatever K would be, then pushing the key into results array. 
   array.slice(0, k).forEach(element => results.push(+element[0]));
-  console.log(array)
   return results
 };
 
-console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2))
+console.log(topKFrequent([3, 3, 4, 5, 1, 1, 1, 2, 2, 3], 2))
