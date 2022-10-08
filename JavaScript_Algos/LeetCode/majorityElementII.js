549651ec5d83a43096092450599e26b6426d55a2
hammyhammer <hammyjacobs@gmail.com>
2022-10-08 19:11:44 -0400

    added explanation code

--- JavaScript_Algos/LeetCode/majorityElementII.js
+++ JavaScript_Algos/LeetCode/majorityElementII.js
@@ -1,10 +1,14 @@
 // Given an array of integers, return all eleents that appear more a third of the size of the array.
 
 let majorityElement = function (nums) {
+  // We have to count the frequency of each number within the array
   let count = {};
+  // Variable for a third of the array.
   let majority = Math.floor(nums.length / 3);
+  // We will return result by pushing whatever numbers occur more than a third of the array size.
   let result = [];
 
+  // For populating our object.
   for (let number of nums) {
     if (count[number]) {
       count[number] += 1;
@@ -14,7 +18,9 @@ let majorityElement = function (nums) {
     }
   }
 
+
   for (let key in count) {
+    // If the key has a value that is greatert than the majority, push it into the results. 
     if (count[key] > majority) {
       result.push(Number(key))
     }
